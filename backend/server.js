8250dc0f3077381c00cc5bd5e68f13b45e9fe89e
tvardhan <tvardhan@vmware.com>
2022-07-19 14:47:28 +0530

    error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,17 @@ const app = require("./app")
 const dotenv = require("dotenv")
 const connectDatabase = require("./config/database")
 
+//handeling uncaught exceptions
+
+process.on("uncaughtException",(err)=>{
+    console.log(`Error; ${err.message}`);
+    console.log("shutting down the server due to uncaught exception");
+    server.close(()=>{
+        process.exit(1);
+    }); 
+});
+
+
 //config
 dotenv.config({path:"backend/config/config.env"})
 
@@ -11,3 +22,12 @@ connectDatabase()
 app.listen(process.env.PORT , ()=>{
     console.log("server runnning on port" , process.env.PORT)
 })
+
+//unhandled promise rejections
+process.on("unhandledRejection",err=>{
+    console.log(`Error : ${err.message}`);
+    console.log('shutting down the server due to undhandled promise rejection');
+    server.close(()=>{
+        process.exit(1);
+    });
+});
