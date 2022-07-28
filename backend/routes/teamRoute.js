efd9fb2303c5cca496e9eca6281a979fb37fa4ab
tvardhan <tvardhan@vmware.com>
2022-07-28 15:10:41 +0530

    authh roles added

--- backend/routes/teamRoute.js
+++ backend/routes/teamRoute.js
@@ -3,15 +3,16 @@ const express = require("express")
 const router = express.Router()
 const { addTeam, getAllTeams, addMember, removeTeam, getTeam, createTeamAssignment, createAllTeams, submitTeamAssignment, getTeamMembers } = require("../controllers/teamController");
 const isAuthenticatedUser = require("../middleware/auth");
+const authorizedRoles = require("../middleware/authRoles");
 
 
 router.route("/getAllTeams").get(isAuthenticatedUser, getAllTeams);
 router.route("/admin/teams/getTeam/{teamId}").get(isAuthenticatedUser, getTeam);
-router.route("/admin/teams/addTeam").post(isAuthenticatedUser, addTeam);
-router.route("/admin/teams/createAssignment/{teamId}").post(isAuthenticatedUser, createTeamAssignment);
-router.route("/admin/teams/removeTeam").delete(isAuthenticatedUser, removeTeam);
-router.route("/admin/teams/createAllTeams").post(isAuthenticatedUser, createAllTeams);
-router.route("/teams/submitTeamAssignment/:teamAssignId").post(isAuthenticatedUser, submitTeamAssignment);
+router.route("/admin/teams/addTeam").post(isAuthenticatedUser, authorizedRoles("admin"), addTeam);
+router.route("/admin/teams/createAssignment/{teamId}").post(isAuthenticatedUser, authorizedRoles("admin"), createTeamAssignment);
+router.route("/admin/teams/removeTeam").delete(isAuthenticatedUser, authorizedRoles("admin"), removeTeam);
+router.route("/admin/teams/createAllTeams").post(isAuthenticatedUser, authorizedRoles("admin"), createAllTeams);
+router.route("/teams/submitTeamAssignment/:teamAssignId").post(isAuthenticatedUser, authorizedRoles("NCG"), submitTeamAssignment);
 router.route("/teams/getTeamMembers").get(isAuthenticatedUser, getTeamMembers);
 
 
